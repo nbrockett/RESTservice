542da4734af7eabdad380a9be8f0ac75307ceb53
nbrockett <n.brockett@gmail.com>
2017-11-16 13:13:33 +0000

    Adding some functionalities for testing purposes.

--- main.py
+++ main.py
@@ -2,8 +2,38 @@ from flask import Flask, url_for
 app = Flask(__name__)
 
 from flask import request
+from flask import Response
+from flask import json
+from flask import jsonify
 
-@app.route('/echo', methods = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+# Adding logging functionality
+import logging
+file_handler = logging.FileHandler('app.log')
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
+
+
+
+@app.route('/hello', methods=['GET'])
+def api_hello():
+
+    # example for logging
+    app.logger.info('informing')
+    app.logger.warning('warning')
+    app.logger.error('error')
+
+    data = {
+        'hello': 'world',
+        'number': 3
+    }
+    resp = jsonify(data)
+    resp.status_code = 200
+    resp.headers['Link'] = 'http://luisrei.com'
+
+    return resp
+
+
+@app.route('/echo', methods=['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
 def api_echo():
     if request.method == 'GET':
         return "ECHO: GET\n"
@@ -17,6 +47,79 @@ def api_echo():
         return "ECHO: DELETE"
     else:
         return "WAAAA?"
+
+@app.route('/messages', methods=['POST'])
+def api_message():
+
+    if request.headers['Content-Type'] == 'text/plain':
+        return "Text Message: " + request.data
+
+    elif request.headers['Content-Type'] == 'application/json':
+        return "JSON Message: " + json.dumps(request.json)
+
+    elif request.headers['Content-Type'] == 'application/octet-stream':
+        f = open('./binary', 'wb')
+        f.write(request.data)
+        f.close()
+        return "Binary message written!"
+    else:
+        return "415 Unsupported Media Type ;)"
+
+## ERROR HANDLING ##
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
+@app.route('/users/<userid>', methods=['GET'])
+def api_users(userid):
+    users = {'1': 'john', '2': 'steve', '3': 'bill'}
+
+    if userid in users:
+        return jsonify({userid: users[userid]})
+    else:
+        return not_found()
+
+## AUTHORIZATION ##
+
+from functools import wraps
+
+def check_auth(username, password):
+    return username == 'admin' and password == 'secret'
+
+def authenticate():
+    message = {'message': "Authenticate."}
+    resp = jsonify(message)
+
+    resp.status_code = 401
+    resp.headers['WWW-Authenticate'] = 'Basic realm="Example"'
+
+    return resp
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth:
+            return authenticate()
+
+        elif not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+
+    return decorated
+
+@app.route('/secrets')
+@requires_auth
+def api_hello2():
+    return "Shhh this is top secret spy stuff!"
 
 if __name__ == '__main__':
     app.run()
